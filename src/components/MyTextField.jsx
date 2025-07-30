007bba35001eeefbe1fe815951a93fa67277b668
saikrishnardy <saikrishnaenugu24@gmail.com>
2025-07-30 15:48:27 +0530

    Initial React frontend with all main pages and components

---
+++ src/components/MyTextField.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MyTextField(props) {
+  return <input {...props} />;
+}
+
+export default MyTextField;
---
+++ src/pages/PestReportsPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PestReportsPage() {
+  return <div>Pest Reports Page</div>;
+}
+
+export default PestReportsPage;
